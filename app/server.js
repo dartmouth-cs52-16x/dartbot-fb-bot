c0cc07b5bee9d74a417318b55b8645c22faf04c1
Alma Wang <almawang96@gmail.com>
2016-07-07 13:41:04 -0400

    first push to deploy on heroku

--- app/server.js
+++ app/server.js
@@ -1,3 +1,42 @@
 // example bot
-
+import botkit from 'botkit';
+// this is es6 syntax for importing libraries
+// in older js this would be: var botkit = require('botkit')
 console.log('starting bot');
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM(err => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
+
+// example hello response
+controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Hello, ${res.user.name}!`);
+    } else {
+      bot.reply(message, 'Hello there!');
+    }
+  });
+});
+
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.replyPublic(message, 'yeah yeah');
+});
