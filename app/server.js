40b50cce0fc6c8e890c70e647a4391c9f6455d38
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2016-08-15 21:56:38 -0400

    deployme

--- app/server.js
+++ app/server.js
@@ -41,34 +41,34 @@ controller.hears(['hello'], 'message_received', (bot, message) => {
   bot.reply(message, 'Hey there.');
 });
 
-controller.hears(['hello'], 'message_received', wit.hears, (bot, message) => {
-  // console.log(message.intents.outcomes.entities);
-});
-
-
-// this is triggered when a user clicks the send-to-messenger plugin
-controller.on('facebook_optin', (bot, message) => {
-  bot.reply(message, 'Welcome to my app!');
-});
-
-controller.hears(['tour'], 'message_received', wit.hears, (bot, message) => {
-  // console.log(message.intents);
-  if (message.intents.outcomes.entities.tour_prompt && message.intents.outcomes.tour_prompt[0].confidence > 0.6) {
-    const topRatedMessage = {
-      'quick_replies': [
-        {
-          'content_type': 'text',
-          'title': 'Red',
-          'payload': 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED',
-        },
-        {
-          'content_type': 'text',
-          'title': 'Green',
-          'payload': 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN',
-        },
-      ],
-    };
-
-    bot.reply(topRatedMessage);
-  }
-});
+// controller.hears(['hello'], 'message_received', wit.hears, (bot, message) => {
+//   // console.log(message.intents.outcomes.entities);
+// });
+//
+//
+// // this is triggered when a user clicks the send-to-messenger plugin
+// controller.on('facebook_optin', (bot, message) => {
+//   bot.reply(message, 'Welcome to my app!');
+// });
+//
+// controller.hears(['tour'], 'message_received', wit.hears, (bot, message) => {
+//   // console.log(message.intents);
+//   if (message.intents.outcomes.entities.tour_prompt && message.intents.outcomes.tour_prompt[0].confidence > 0.6) {
+//     const topRatedMessage = {
+//       'quick_replies': [
+//         {
+//           'content_type': 'text',
+//           'title': 'Red',
+//           'payload': 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED',
+//         },
+//         {
+//           'content_type': 'text',
+//           'title': 'Green',
+//           'payload': 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN',
+//         },
+//       ],
+//     };
+//
+//     bot.reply(topRatedMessage);
+//   }
+// });
