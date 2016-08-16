8cdbbea73c4148eb53c496d2931b9f22169f3439
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2016-08-16 13:27:19 -0400

    dep

--- app/server.js
+++ app/server.js
@@ -66,56 +66,43 @@ controller.hears(['hello'], 'message_received', (bot, message) => {
 // });
 //
 controller.hears(['tour'], 'message_received', (bot, message) => {
-  // function confirmSurveyTaking(response, convo) {
-  //   const topRatedMessage = {
-  //     'text': 'You went on the Dartmouth tour? Would you like to give us some quick feedback to help improve it?',
-  //     'quick_replies': [
-  //       {
-  //         'content_type': 'text',
-  //         'title': 'Yes',
-  //         'payload': 'YES_FEEDBACK',
-  //       },
-  //       {
-  //         'content_type': 'text',
-  //         'title': 'No',
-  //         'payload': 'NO_FEEDBACK',
-  //       },
-  //     ],
-  //   };
-  //
-  //   // bot.reply(message, topRatedMessage);
-  //   convo.ask(topRatedMessage, [
-  //     {
-  //       pattern: bot.utterances.yes,
-  //       callback(resp, conv) {
-  //         convo.say('Well I would probably use Google');
-  //         // getFoodType(resp, conv);
-  //         // convo.next();
-  //       },
-  //     },
-  //     {
-  //       pattern: bot.utterances.no,
-  //       callback(resp, conv) {
-  //         convo.say('No? Well ask me anytime, I\'ll be around here somewhere!');
-  //         convo.next();
-  //       },
-  //     },
-  //     {
-  //       default: true,
-  //       callback(resp, conv) {
-  //         convo.say('I\'ll take that as a no? Well, ask anytime!');
-  //         convo.next();
-  //       },
-  //     },
-  //   ]);
-  // }
-  // console.log('out here');
-  console.log(message.intents[0].entities.tour_prompt[0].confidence);
-  // console.log(message.intents[0].outcomes);
-  // console.log(message.intents);
+  function confirmTour(response, convo) {
+    const topRatedMessage = {
+      'text': 'You went on the Dartmouth tour? Would you like to give us some quick feedback to help improve it?',
+      'quick_replies': [
+        {
+          'content_type': 'text',
+          'title': 'Yes',
+          'payload': 'YES_FEEDBACK',
+        },
+        {
+          'content_type': 'text',
+          'title': 'No',
+          'payload': 'NO_FEEDBACK',
+        },
+      ],
+    };
 
+    convo.ask(topRatedMessage, [
+      {
+        pattern: bot.utterances.no,
+        callback(resp, conv) {
+          convo.say('No? Well ask me anytime, I\'ll be around here somewhere!');
+          convo.next();
+        },
+      },
+      {
+        default: true,
+        callback(resp, conv) {
+          convo.say('I\'ll take that as a no? Well, ask anytime!');
+          convo.next();
+        },
+      },
+    ]);
+  }
   // check if this sentence with tour in it is above our Wit.ai ML algorithm's 65% confidence threshhold for being related to finishing the tour
   if (message.intents[0] && message.intents[0].entities && message.intents[0].entities.tour_prompt && message.intents[0].entities.tour_prompt[0].confidence > 0.6) {
+    bot.startConversation(message, confirmTour);
     const topRatedMessage = {
       'text': 'You went on the Dartmouth tour? Would you like to give us some quick feedback to help improve it?',
       'quick_replies': [
