0a93dcd2eaecab7684df65ac928c58dc919677cc
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2016-08-22 19:35:11 -0400

    get location from backend

--- app/server.js
+++ app/server.js
@@ -65,7 +65,7 @@ controller.on('message_received', (bot, message) => {
 
 function returnNearestLocation(bot, message, coordinates) {
   bot.reply(message, 'Beep boop. Finding your nearest tour location...');
-	const fields = { lat: coordinates.lat, lon: coordinates.long };
+	const fields = { lat: coordinates.lat, long: coordinates.long };
 	axios.put(`${ROOT_URL}/data/closest`, fields)
   	.then(response => {
     	console.log('got a response');
