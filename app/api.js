5fa1e8120cf8671f897014b30516adba8bc01cc4
Alma Wang <almawang@almas-mbp.wireless.dartmouth.edu>
2016-08-29 17:47:55 -0400

    random survey

--- app/api.js
+++ app/api.js
@@ -4,6 +4,7 @@ const ROOT_URL = 'http://dartmouthbot.herokuapp.com/api';
 
 export function getSurveys() {
   axios.get(`${ROOT_URL}/survey`).then(response => {
+    console.log(response);
     return (response.data);
   }).catch(error => {
     console.log(error);
